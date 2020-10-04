fe38ea353a7f104a17e8508c8fc9b636c5b9f510
bit0101jp <bit0101jp@foo>
2020-10-04 12:13:36 +0900

    eview of program structure

--- main.js
+++ main.js
@@ -1,115 +1,81 @@
-// setup the control gui
+// setup the control panel
 const controls = new function () {
-    this.amount = 2;
-    this.upperPull = 13;
+    // default value for slider
+    this.thickness = 8;
+    this.upperPull = 10;
     this.lowerPull = 5;
 
     this.asGeom = function () {
-        // remove the old plane
+        // remove the old shape
         scene.remove(shape);
-        // create a new one
 
         const options = {
-            amount: controls.amount,
-            bevelEnabled: false,
+            // Specify the thickness with the slider
+            amount: controls.thickness,
+            // bevel off
+            bevelEnabled: false
         };
 
+        // create the new shape
         shape = createMesh(new THREE.ExtrudeGeometry(drawShape(), options));
-        // add it to the scene.
         scene.add(shape);
     };
-
 };
 
-function createLine(shape, spaced) {
-    if (!spaced) {
-        const mesh = new THREE.Line(shape.createPointsGeometry(), new THREE.LineBasicMaterial({
-            color: 0xff0000,
-            linewidth: 5
-        }));
-        return mesh;
-    } else {
-        const mesh = new THREE.Line(shape.createSpacedPointsGeometry(20), new THREE.LineBasicMaterial({
-            color: 0xff0000,
-            linewidth: 1
-        }));
-        return mesh;
-    }
 
-}
-
-// draw while rotating the solid figure
+// Create basic shapes for extruded geometry
 function drawShape() {
 
     // create a basic shape
     const shape = new THREE.Shape();
 
-    // startpoint
+    // starting point
     shape.moveTo(-10, 0);
 
-    // straight line upwards
-//    shape.lineTo(10, 40);
+    // vertical line
     shape.lineTo(-10, 30);
+    // horizon line
     shape.lineTo( 0, 30);
 
-    // the top of the figure, curve to the right
-//    shape.bezierCurveTo(15, 25, 25, 25, 30, 40);
-
-    // spline back down
+    // spline curve
     shape.splineThru(
-//        [new THREE.Vector2(32, 30),
-            [new THREE.Vector2(controls.upperPull, 20),
-                new THREE.Vector2(controls.lowerPull, 10),
-                new THREE.Vector2(13, 0),
-            ]);
+        [new THREE.Vector2(controls.upperPull, 20),
+         new THREE.Vector2(controls.lowerPull, 10),
+         new THREE.Vector2(13, 0),
+        ]);
 
-    // // curve at the bottom
-    // shape.quadraticCurveTo(20, 15, 10, 10);
+    // vertical line
     shape.lineTo(-10, 0);
 
-    // // add 'eye' hole one
-    // const hole1 = new THREE.Path();
-    // hole1.absellipse(16, 24, 2, 3, 0, Math.PI * 2, true);
-    // shape.holes.push(hole1);
+    // hole
+    const hole = new THREE.Path();
+    hole.absellipse(-5, 19, 4, 4, 0, Math.PI * 2, true);
+    shape.holes.push(hole);
 
-    // // add 'eye hole 2'
-    // const hole2 = new THREE.Path();
-    // hole2.absellipse(23, 24, 2, 3, 0, Math.PI * 2, true);
-    // shape.holes.push(hole2);
-
-    // // add 'mouth'
-    // const hole3 = new THREE.Path();
-    // hole3.absarc(20, 16, 2, 0, Math.PI, true);
-    // shape.holes.push(hole3);
-
-    // return the shape
     return shape;
 }
+
 
 // create a mesh of geometry and materials
 function createMesh(geom) {
 
     geom.applyMatrix(new THREE.Matrix4().makeTranslation(0, 0, 0));
 
-    // assign two materials
+    // create a mesh material
     const meshMaterial = new THREE.MeshNormalMaterial({
-        transparent: true,
-        opacity: 0.8
+        transparent: false,
+//        opacity: 1.0
     });
 
-    //  meshMaterial.side = THREE.DoubleSide;
-    const wireFrameMat = new THREE.MeshBasicMaterial();
-    wireFrameMat.wireframe = true;
-
-    // create a multimaterial
+    // create a mesh
     const mesh = THREE.SceneUtils.createMultiMaterialObject(geom, [meshMaterial]);
 
     return mesh;
 }
 
+
 function rendering() {
 
-//    shape.rotation.y = step += 0.01;
     shape.rotation.y = step += 0.02;
 
     // render using requestAnimationFrame
@@ -120,22 +86,43 @@ function rendering() {
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+// create a render
 const webGLRenderer = new THREE.WebGLRenderer();
 webGLRenderer.setClearColor(new THREE.Color(0xEEEEEE));
 webGLRenderer.setSize(window.innerWidth, window.innerHeight);
-webGLRenderer.shadowMap.enabled = true;
+
+// x, y, z axes from the origin (for debug)
+const axes = new THREE.AxisHelper(35);
+scene.add(axes);
 
 let shape = createMesh(new THREE.ShapeGeometry(drawShape()));
-// add the sphere to the scene
 scene.add(shape);
 
-// position and point the camera to the center of the scene
+const base = new THREE.Mesh(new THREE.BoxGeometry(100, 100, 0.1, 30), new THREE.MeshPhongMaterial({
+    color: 0x004400
+}));
+
+base.position.x = 0;
+base.position.y = -10;
+base.rotation.x = -0.4 * Math.PI;
+scene.add(base);
+
+// create a ambient light
+const ambiLight = new THREE.AmbientLight(0x777777);
+scene.add(ambiLight);
+
+// create a spotlight
+const light = new THREE.SpotLight(0xffffff);
+light.position.set(0, 60, 0);
+light.intensity = 2.0;
+scene.add(light);
+
+// create a camera
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.x = 0;
-camera.position.y = 50;
-camera.position.z = 100;
-//camera.lookAt(new THREE.Vector3(20, 20, 0));
+camera.position.y = 40;
+camera.position.z = 120;
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 // add the output destination of the renderer as an HTML element
@@ -144,11 +131,11 @@ document.getElementById("WebGL-div").appendChild(webGLRenderer.domElement);
 // call the render function
 let step = 0;
 
+// create a contorl panel
 const gui = new dat.GUI();
-gui.add(controls, 'amount', 0, 40).onChange(controls.asGeom);
-gui.add(controls, 'upperPull', -5, 20).onChange(controls.asGeom);
-gui.add(controls, 'lowerPull', -5, 20).onChange(controls.asGeom);
-//gui.add(controls, 'curveSegments', 1, 30).step(1).onChange(controls.asGeom);
+gui.add(controls, 'thickness', 0, 40, 0.1).onChange(controls.asGeom);
+gui.add(controls, 'upperPull', 0, 20, 0.1).onChange(controls.asGeom);
+gui.add(controls, 'lowerPull', -5, 20, 0.1).onChange(controls.asGeom);
 
 controls.asGeom();
 
